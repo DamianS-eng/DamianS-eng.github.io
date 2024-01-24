34c416c41f79fc8b8522195ca446d5bd0796871e
Damian <damiansuaz@gmail.com>
2024-01-24 10:43:35 -0500

    Update index.js
    Add nav button and behavior for nav list

--- docs/index.js
+++ docs/index.js
@@ -2,6 +2,7 @@ const debug = true;
 const tabs = document.querySelectorAll('.tab_btn');
 const content = document.querySelectorAll('.content');
 const line = document.querySelector('#active-line');
+const navList = document.querySelector("#navList");
 let activechosen = "";
 if(debug) {console.log(tabs);}
 
@@ -22,6 +23,19 @@ function chosen(e, indic) {
       pieceOfContent.classList.add('active');
     };
   });
+}
+function ToggleNav(e) {
+  if(e.getAttribute("active")) {
+    e.removeAttribute("active");
+    e.innerHTML = "<";
+    navList.removeAttribute("active");
+    console.log("Removed.");
+    return;
+  }
+  e.setAttribute("active", true);
+  e.innerHTML = ">";
+  navList.setAttribute("active", true)
+  console.log("Making Active.");
 }
 
 window.addEventListener('resize', function(event) {
