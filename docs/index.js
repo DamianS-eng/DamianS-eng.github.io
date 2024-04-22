11581b78fa5ba154d4499adc3d2bf9c2d74ffcb4
Damian <damiansuaz@gmail.com>
2024-04-21 20:10:40 -0400

    Refresh the nav bar
    Less choices looks nicer, drop all projects in a list in the content-box

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-const debug = false;
+const debug = true;
 const tabs = document.querySelectorAll('.tab_btn');
 const content = document.querySelectorAll('.content');
 const line = document.querySelector('#active-line');
@@ -7,12 +7,13 @@ let activechosen = "";
 if(debug) {console.log(tabs);}
 
 function chosen(e, indic) {
-  if(debug) {console.log(e);}
-  if(debug) {console.log(indic);}
+  //if(debug) {console.log(e);}
+  //if(debug) {console.log(indic);}
   tabs.forEach(tab=>{tab.classList.remove('active')});
   content.forEach(pieceOfContent=>{pieceOfContent.classList.remove('.active');});
   if(debug) {line.classList.add('invis');}
   activechosen = document.querySelector(e);
+  if(debug) {console.log(activechosen)}
   activechosen.classList.add('active');
   line.style.width = activechosen.offsetWidth + "px";
   line.style.left = activechosen.offsetLeft + "px";
